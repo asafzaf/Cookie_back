e7db1e8d979dcca352b91549da86c514c3a1e6fb
asafzaf <asaf.zafrir@gmail.com>
2024-10-20 22:42:39 +0300

    added departments

--- db/initItems.js
+++ db/initItems.js
@@ -12,15 +12,24 @@ mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}`, {
   useUnifiedTopology: true,
 });
 
-const json = require("./items.json");
+const itemsJson = require("./items.json");
+const departmentsJson = require("./departments.json");
 
 const Item = require("../models/item.model");
 const ShoppingList = require("../models/shoppingList.model");
+const Department = require("../models/department.model");
 
 async function initItems() {
   try {
     await Item.deleteMany({});
-    await Item.insertMany(json.items);
+    const departments = await Department.find();
+    itemsJson.items.forEach((item) => {
+      item.department = departments.find(
+        (department) => department.name.en === item.department
+      )?._id || null;
+    });
+    await Item.insertMany(itemsJson.items);
+    // await Item.insertMany(itemsJson.items);
     console.log("Items added successfully");
   } catch (error) {
     console.error("Error adding items:", error);
@@ -28,14 +37,30 @@ async function initItems() {
 }
 
 async function initShoppingList() {
-    try {
-        await ShoppingList.deleteMany({});
-        console.log("Shopping lists removed successfully");
-    } catch (error) {
-        console.error("Error adding shopping lists:", error);
-    }
-    }
+  try {
+    await ShoppingList.deleteMany({});
+    console.log("Shopping lists removed successfully");
+  } catch (error) {
+    console.error("Error adding shopping lists:", error);
+  }
+}
 
-initItems();
+async function initDepartments() {
+  try {
+    await Department.deleteMany({});
+    await Department.insertMany(departmentsJson.departments);
+    console.log("Departments added successfully");
+  } catch (error) {
+    console.error("Error adding departments:", error);
+  }
+}
+let main = async () => {
+  await initDepartments();
+  await initItems();
+  await initShoppingList();
+};
 
-initShoppingList();
+main().then(() => {
+  mongoose.connection.close();
+  console.log("Connection closed");
+});
