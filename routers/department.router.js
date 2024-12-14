b5e90b65226a6314fae15ce22537f0b41161ef80
Asafz <asaf.zafrir@gmail.com>
2024-12-14 17:14:52 +0200

    added to route departments.

--- routers/department.router.js
+++ routers/department.router.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const authController = require("../controllers/auth.controller");
 const DepartmentController = require("../controllers/department.controller");
 const router = express.Router();
 
-router.get("/", DepartmentController.getAllDepartments);
-router.get("/:id", DepartmentController.getDepartment);
-router.post("/", DepartmentController.createDepartment);
-router.put("/:id", DepartmentController.updateDepartment);
-router.delete("/:id", DepartmentController.deleteDepartment);
+router.get("/", authController.protect, DepartmentController.getAllDepartments);
+router.get("/:id", authController.protect, DepartmentController.getDepartment);
+router.post("/", authController.protect, DepartmentController.createDepartment);
+router.put(
+  "/:id",
+  authController.protect,
+  DepartmentController.updateDepartment
+);
+router.delete(
+  "/:id",
+  authController.protect,
+  DepartmentController.deleteDepartment
+);
 
 module.exports = router;
